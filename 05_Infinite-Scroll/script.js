a93a2d3d205454951b2262360fb1463657a7dfb7
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-06-11 08:56:42 -0400

    removed secret

--- 05_Infinite-Scroll/script.js
+++ 05_Infinite-Scroll/script.js
@@ -1,5 +1,5 @@
 const count = 10;
-const apiKey = '7rCQV-KFeDxp_tFkoKBmI16lDC9MMbMVJZPkibfqqCo';
+const apiKey = '';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Get Photos from Unsplash API
