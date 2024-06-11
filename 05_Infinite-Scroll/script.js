21bff81ed40dc04ab8650e7fd6e9de14ed3bab25
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-06-11 12:00:09 -0400

    javascript to display data

--- 05_Infinite-Scroll/script.js
+++ 05_Infinite-Scroll/script.js
@@ -1,13 +1,54 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
 const count = 10;
-const apiKey = '';
+const apiKey = '7rCQV-KFeDxp_tFkoKBmI16lDC9MMbMVJZPkibfqqCo';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+let photos = [];
+
+//Helper function to Set Attributes on DOM Elements
+function setAttributes(element, attributeObj) {
+  for (const key in attributeObj) {
+    element.setAttribute(key, attributeObj[key]);
+  }
+}
+
+//Create Elements for links & photos, Add to Dom
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photos.forEach((photo) => {
+    // create <a> to link to unsplash
+    const anchorElement = document.createElement('a');
+    // anchorElement.setAttribute('href', photo.links.html);
+    setAttributes(anchorElement, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // anchorElement.setAttribute('target', '_blank');
+    //create <img> for photo
+    const imgElement = document.createElement('img');
+    // imgElement.setAttribute('src', photo.urls.regular);
+    // imgElement.setAttribute('alt', photo.alt_description);
+    setAttributes(imgElement, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // imgElement.setAttribute('title', photo.alt_description);
+    //Put <img> inside <a>, then put both inside imageContainer Element
+    anchorElement.appendChild(imgElement);
+    imageContainer.appendChild(anchorElement);
+  });
+}
 
 //Get Photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
+    photos = data;
+    displayPhotos();
   } catch (error) {
     //Catch Error here
   }
